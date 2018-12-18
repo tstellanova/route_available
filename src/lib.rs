7ad8a7546e7b73ca67565a3f02a7776660f72359
Todd Stellanova <tstellanova@gmail.com>
2018-12-18 04:48:39 +0000

    initial commit...

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+/*
+Copyright (c) 2018 Todd Stellanova
+
+LICENSE: See LICENSE file
+*/
+
+extern crate regex;
+use regex::Regex;
+
+#[macro_use]
+extern crate lazy_static;
+
+use std::process::Command;
+
+/// Returns true if connected to a network (a route is available)
+pub fn route_available() -> bool  {
+  lazy_static! {
+    static ref ROUTE_MATCHER: Regex  = Regex::new(r"(default)").expect("bad regex");
+  }
+
+  let out = Command::new("ip")
+    .arg("route")
+    .arg("show")
+    .output()
+    .expect("couldn't run command");
+
+  let text = String::from_utf8_lossy(&out.stdout);
+
+  ROUTE_MATCHER.is_match(&text)
+}
+
+#[cfg(test)]
+mod tests {
+use route_available ;
+
+    #[test]
+    fn it_works() {
+      assert!(route_available());
+    }
+}
